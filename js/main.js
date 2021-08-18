f2f52aca6383f2c17e57ad6a65974047133bb245
hemmingson <287969392@qq.com>
2021-08-18 21:18:48 +0800

    feat: implement tag selection feature

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 $('.pic-rap').fadeIn('slow').css({ display: 'flex' });
+
+$('.tag').on('click', (e) => {
+  $('.selected').removeClass('selected');
+  $(e.target).addClass('selected');
+
+  const category = e.target.classList[1];
+  if (category === 'all') {
+    $('article > a').show();
+    return;
+  }
+  $('article > a').hide();
+  $(`.${category}`).show();
+});
